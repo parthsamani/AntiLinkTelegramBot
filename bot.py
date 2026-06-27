252997072c4cc0f1a8d0b66d2acd77c2a74c5187
parthsamani <parthsamani@zohomail.in>
2026-06-27 10:23:37 +0530

    Change Telegram bot token
    Updated the bot token for the Telegram bot.

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 # Yahan apna BotFather ka token paste karna
-BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8876100729:AAFclBFxBE8QgclXgWwM5VDCcJwy_w65dYM"
 
 # Link detect karne ke liye regex
 LINK_PATTERN = re.compile(
